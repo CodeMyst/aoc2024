f3b5a1b5e91f0be9a95f04bf4004121e6a05c74e
CodeMyst <code@myst.rs>
2024-11-29 11:53:21 +0100

    fixed indentation

--- puzzles/00/puzzle.ts
+++ puzzles/00/puzzle.ts
@@ -2,18 +2,18 @@ import type { PuzzleSolver } from "../../core";
 
 export const solve: PuzzleSolver = {
     part1: {
-	expectedResult: { example: 3, full: 12 },
+        expectedResult: { example: 3, full: 12 },
 
-	solve: (input: string): number => {
-	    return input.trim().length;
-	}
+        solve: (input: string): number => {
+             return input.trim().length;
+        }
     },
 
     part2: {
-	expectedResult: { example: 4, full: 13 },
+        expectedResult: { example: 4, full: 13 },
 
-	solve: (input: string): number => {
-	    return input.trim().length + 1;
-	}
+        solve: (input: string): number => {
+             return input.trim().length + 1;
+        }
     }
 };
