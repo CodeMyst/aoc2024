d104915a55c27f9f5e5416e42c55846cbc89316b
CodeMyst <code@myst.rs>
2024-12-03 10:53:39 +0100

    simplified day 03 a bit

--- puzzles/03/puzzle.ts
+++ puzzles/03/puzzle.ts
@@ -5,10 +5,8 @@ export const solve: PuzzleSolver = {
         expectedResult: { example: 161, full: 182_619_815 },
 
         solve: (input: string): number => {
-            return input.split('\n')
-                        .map(l => l.matchAll(/mul\((\d+),(\d+)\)/g)
-                                   .map(m => Number(m[1]) * Number(m[2]))
-                                   .reduce((a, n) => a + n, 0))
+            return input.matchAll(/mul\((\d+),(\d+)\)/g)
+                        .map(m => Number(m[1]) * Number(m[2]))
                         .reduce((a, n) => a + n, 0);
         }
     },
@@ -17,19 +15,14 @@ export const solve: PuzzleSolver = {
         expectedResult: { example: 48, full: 80_747_545 },
 
         solve: (input: string): number => {
-            const lines = input.split('\n');
+            const matches = input.matchAll(/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g);
 
             let sum = 0;
-
             let doMul = true;
-            for (const line of lines) {
-                const matches = line.matchAll(/mul\((\d+),(\d+)\)|do\(\)|don't\(\)/g);
-
-                for (const match of matches) {
-                    if (match[0] === `don't()`) doMul = false;
-                    else if (match[0] === 'do()') doMul = true;
-                    else if (doMul) sum += Number(match[1]) * Number(match[2]);
-                }
+            for (const match of matches) {
+                if (match[0] === `don't()`) doMul = false;
+                else if (match[0] === 'do()') doMul = true;
+                else if (doMul) sum += Number(match[1]) * Number(match[2]);
             }
 
             return sum;
