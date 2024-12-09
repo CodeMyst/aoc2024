f41e833d883d1b3f3f33acc5a82dffd564065afd
CodeMyst <code@myst.rs>
2024-12-09 16:55:17 +0100

    day 09 improved

--- puzzles/09/puzzle.ts
+++ puzzles/09/puzzle.ts
@@ -1,41 +1,82 @@
 import type { PuzzleSolver } from '../../core';
 
+type Block = {
+    index: number;
+    id: number;
+    length: number;
+};
+
+const parseBlocks = (input: string): Block[] => {
+    const inputNumbers = input.trim().split('');
+    const blocks: Block[] = [];
+    let id = 0;
+    for (let i = 0; i < inputNumbers.length; i++) {
+        if (i % 2 === 0) {
+            blocks.push({ id, length: Number(inputNumbers[i]), index: i });
+            id++;
+        } else {
+            blocks.push({ id: -1, length: Number(inputNumbers[i]), index: i });
+        }
+    }
+
+    return blocks;
+};
+
+const calculateChecksum = (blocks: Block[]): number => {
+    let result = 0;
+    let currentId = 0;
+    for (let i = 0; i < blocks.length; i++) {
+        if (blocks[i].id !== -1) {
+            for (let s = 0; s < blocks[i].length; s++) result += (currentId + s) * blocks[i].id;
+        }
+
+        currentId += blocks[i].length;
+    }
+
+    return result;
+};
+
 export const solve: PuzzleSolver = {
     part1: {
         expectedResult: { example: 1928, full: 6_461_289_671_426 },
 
         solve: (input: string): number => {
-            const inputNumbers = input.trim().split('');
-            const expanded: number[] = [];
-            let id = 0;
-            for (let i = 0; i < inputNumbers.length; i++) {
-                if (i % 2 === 0) {
-                    const blocks = Array.from({ length: Number(inputNumbers[i])}, () => id);
-                    expanded.push(...blocks);
-                    id++;
+            const blocks = parseBlocks(input);
+
+            for (let i = blocks.length - 1; i >= 0; i--) {
+                if (blocks[i].id === -1) continue;
+
+                const firstFreeBlockIndex = blocks.slice(0, i).findIndex(b => b.id === -1);
+
+                if (firstFreeBlockIndex === -1) break;
+
+                const firstFreeBlock = blocks[firstFreeBlockIndex];
+
+                if (firstFreeBlock.length === blocks[i].length) {
+                    firstFreeBlock.id = blocks[i].id;
+                    blocks[i].id = -1;
+                } else if (firstFreeBlock.length > blocks[i].length) {
+                    firstFreeBlock.id = blocks[i].id;
+                    const remainingFree = firstFreeBlock.length - blocks[i].length;
+                    firstFreeBlock.length = blocks[i].length;
+
+                    blocks[i].id = -1;
+
+                    const newFreeBlock: Block = { id: -1, length: remainingFree, index: firstFreeBlock.index };
+                    blocks.splice(firstFreeBlockIndex + 1, 0, newFreeBlock);
                 } else {
-                    const blocks = Array.from({ length: Number(inputNumbers[i])}, () => -1);
-                    expanded.push(...blocks);
+                    firstFreeBlock.id = blocks[i].id;
+                    const remainingFilled = blocks[i].length - firstFreeBlock.length;
+                    blocks[i].length = remainingFilled;
+
+                    const newFreeBlock: Block = { id: -1, length: firstFreeBlock.length, index: blocks[i].index };
+                    blocks.splice(i + 1, 0, newFreeBlock);
+
+                    i++;
                 }
             }
 
-            for (let i = expanded.length - 1; i >= 0; i--) {
-                if (expanded[i] !== -1) {
-                    const firstFreeIndex = expanded.findIndex(b => b === -1);
-                    if (firstFreeIndex === -1 || firstFreeIndex >= i) break;
-                    expanded[firstFreeIndex] = expanded[i];
-                    expanded[i] = -1;
-                }
-            }
-
-            let totalId = 0;
-            for (let i = 0; i < expanded.length; i++) {
-                if (expanded[i] !== -1) {
-                    totalId += i * expanded[i];
-                }
-            }
-
-            return totalId;
+            return calculateChecksum(blocks);
         }
     },
 
@@ -43,62 +84,36 @@ export const solve: PuzzleSolver = {
         expectedResult: { example: 4, full: 6_488_291_456_470 },
 
         solve: (input: string): number => {
-            const inputNumbers = input.trim().split('');
-            const expanded: number[] = [];
-            let id = 0;
-            for (let i = 0; i < inputNumbers.length; i++) {
-                if (i % 2 === 0) {
-                    const blocks = Array.from({ length: Number(inputNumbers[i])}, () => id);
-                    expanded.push(...blocks);
-                    id++;
-                } else {
-                    const blocks = Array.from({ length: Number(inputNumbers[i])}, () => -1);
-                    expanded.push(...blocks);
-                }
-            }
+            const blocks = parseBlocks(input);
 
-            let currentId = expanded.toSorted()[expanded.length - 1];
-            while (currentId > 0) {
-                const blockStartIndex = expanded.findIndex(b => b === currentId);
-                const blockEndIndex = expanded.findLastIndex(b => b === currentId);
-                const requiredSpace = blockEndIndex - blockStartIndex;
-
-                let freeStartIndex = -1;
-                let freeCount = 0;
-                for (let i = 0; i < blockStartIndex; i++) {
-                    if (expanded[i] === -1) {
-                        if (freeStartIndex === -1) {
-                            freeStartIndex = i;
-                        }
-                        freeCount++;
-
-                        if (freeCount === requiredSpace + 1) {
-                            break;
-                        }
-                    } else {
-                        freeStartIndex = -1;
-                        freeCount = 0;
-                    }
-                }
-
-                if (freeStartIndex !== -1 && freeCount === requiredSpace + 1) {
-                    for (let i = 0; i <= requiredSpace; i++) {
-                        expanded[freeStartIndex + i] = expanded[blockStartIndex + i];
-                        expanded[blockStartIndex + i] = -1;
-                    }
-                }
-
+            let currentId = blocks.toSorted((a, b) => a.id - b.id)[blocks.length - 1].id;
+            while (currentId >= 0) {
+                const block = blocks.find(b => b.id === currentId);
                 currentId--;
-            }
+                if (!block) continue;
 
-            let totalId = 0;
-            for (let i = 0; i < expanded.length; i++) {
-                if (expanded[i] !== -1) {
-                    totalId += i * expanded[i];
+                const firstFreeBlockIndex = blocks.findIndex(b => b.id === -1 && b.index < block.index && b.length >= block.length);
+
+                if (firstFreeBlockIndex === -1) continue;
+
+                const firstFreeBlock = blocks[firstFreeBlockIndex];
+
+                if (firstFreeBlock.length === block.length) {
+                    firstFreeBlock.id = block.id;
+                    block.id = -1;
+                } else if (firstFreeBlock.length > block.length) {
+                    firstFreeBlock.id = block.id;
+                    const remainingFree = firstFreeBlock.length - block.length;
+                    firstFreeBlock.length = block.length;
+
+                    block.id = -1;
+
+                    const newFreeBlock: Block = { id: -1, length: remainingFree, index: firstFreeBlock.index };
+                    blocks.splice(firstFreeBlockIndex + 1, 0, newFreeBlock);
                 }
             }
 
-            return totalId;
+            return calculateChecksum(blocks);
         }
     }
 };
